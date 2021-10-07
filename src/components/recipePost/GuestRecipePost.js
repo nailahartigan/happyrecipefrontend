9dbdf804b908cafa36051c4d46144cd85850d13c
adeelzhub <adeelijaz@hotmail.com>
2021-10-07 10:52:57 +0100

    initial commit

---
+++ src/components/recipePost/GuestRecipePost.js
@@ -0,0 +1,29 @@
+import "./GuestHomePage.css";
+import React from "react";
+import Navbar from "../navbar/Index";
+import american from "../../pictures/american.PNG";
+import food from "https://unsplash.com/photos/w6ftFbPCs9I";
+
+function GuestRecipePost() {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="post">
+        <div className="cuisine1">
+          <img className="food" src={food} alt="food" />
+          <img className="american" src={american} alt="American" />
+          <div className="AMERICAN">AMERICAN</div>
+          <img className="asian" src="/asian.PNG" alt="Asian" />
+          <div className="ASIAN">ASIAN</div>
+          <img className="barbeque" src="/barbeque.PNG" alt="Barbeque" />
+          <div className="BARBEQUE">BARBEQUE</div>
+          <img className="german" src="/german.PNG" alt="German" />
+          <div className="GERMAN">GERMAN</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GuestRecipePost;
